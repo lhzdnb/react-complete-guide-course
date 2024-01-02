f9d8adc4c23e18705ac0cbf80d0a569d21be96c3
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-02 02:01:50 -0800

    Rendering Content Conditionally

--- 03 React Essentials/src/App.jsx
+++ 03 React Essentials/src/App.jsx
@@ -6,12 +6,24 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedBtn) {
     // selectedBtn => Components, JSX, Props, State
     setSelectedTopic(selectedBtn);
   }
+
+  let tabContent = selectedTopic ? (
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  ) : (
+    <p>Please select a topic.</p>
+  );
 
   return (
     <div>
@@ -40,13 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
