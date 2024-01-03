2c83368b19079d2095fa488feae89055daac4391
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-02 18:33:09 -0800

    Working with State

--- 04 React Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
+++ 04 React Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,12 @@
+import Player from "./components/Player.jsx";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">x</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">0</span>
-          </li>
+          <Player name="Player 1" symbol="x" />
+          <Player name="Player 2" symbol="0" />
         </ol>
         GAME BOARD
       </div>
