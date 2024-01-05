9d581d9e37bbe00765e99b8079ed57a77552648b
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-04 23:58:19 -0800

    Ref

--- 08 Working with Refs Portals/src/components/Player.jsx
+++ 08 Working with Refs Portals/src/components/Player.jsx
@@ -6,11 +6,12 @@ export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>{enteredPlayerName || "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName || "unknown entity"}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
