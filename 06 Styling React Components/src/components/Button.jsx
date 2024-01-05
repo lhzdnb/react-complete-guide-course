34c02ee8ff25638c07ee69c0a89469b916ba8899
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-04 21:41:54 -0800

    Tailwind

--- 06 Styling React Components/src/components/Button.jsx
+++ 06 Styling React Components/src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, ...props }) {
   return (
     <button
-      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-stone-400 hover:bg-amber-500 active:bg-amber-800"
+      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-500 hover:bg-amber-600"
       {...props}
     >
       {children}
